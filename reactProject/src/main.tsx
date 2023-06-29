427067383b5b030e9d997b0bb1ee332d78696b5d
bottzo <jordiet1999@gmail.com>
2023-06-29 20:14:15 +0200

    painting an array

--- reactProject/src/main.tsx
+++ reactProject/src/main.tsx
@@ -15,9 +15,16 @@ function getFullName(user: tUser)
   return `${user.name} ${user.surname}`;
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const elements = (
   <div>
     <h1>Hi react from {getFullName(user)}</h1>
     <h1>Hi react from {getFullName({name:"Bob", surname:"Smth"})}</h1>
   </div>
+);
+
+const myArr = [1, 3*4, null, " Hola"];
+const element = <h1>{myArr}</h1>;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+element
 );
