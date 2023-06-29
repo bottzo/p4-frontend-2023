eef16959f0eda03d64a3fffdb82d681ebb7452d8
bottzo <jordiet1999@gmail.com>
2023-06-29 19:24:46 +0200

    added js expresion inside jsx expresion using {}

--- reactProject/src/main.tsx
+++ reactProject/src/main.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+//import App from './App.tsx'
 import './index.css'
 
+type tUser = {name:string, surname:string}
+
+let user : tUser = {
+  name:"James",
+  surname: "bond"
+}
+
+function GetFullName(user: tUser)
+{
+  return `${user.name} ${user.surname}`
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <h1>Hi react</h1>
+  <h1>Hi react from {GetFullName(user)}</h1>
 );
