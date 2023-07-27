cfe341a4acc21c209ae31d79506252b6c546ac66
bottzo <45202057+bottzo@users.noreply.github.com>
2023-07-27 13:48:23 +0200

    Added events

--- myreactapp/src/main.tsx
+++ myreactapp/src/main.tsx
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom/client'
 import Clock from "./Clock"
 import Todos from './Todos'
 
+class ActionLink extends React.Component{
+  constructor(props)
+  {
+    super(props);
+    this.state = {active: true};
+  }
+  render(): React.ReactNode {
+    return(
+    <>
+      <a href="#" onClick={(e) => {e.preventDefault(); this.setState({active: !this.state.active})}}>{(this.state.active) ? "Hide" : "Show"}</a>
+      {this.state.active && this.props.obj};
+    </>
+    );
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   (<>
     <h1>Hi</h1>
-    <Clock color="blue"/>
+    <ActionLink obj={<Clock color="blue"/>}/>
     <Todos/>
   </>)
 )
