b8f8a8bd26acb82ec921c7f78aff8a57d98fde68
bottzo <45202057+bottzo@users.noreply.github.com>
2023-07-29 17:23:19 +0200

    added select to form

--- myreactapp/src/form.tsx
+++ myreactapp/src/form.tsx
@@ -4,19 +4,29 @@ export default class NameForm extends React.Component{
     constructor(props)
     {
         super(props);
-        this.state = {value:""}; 
+        this.state = {iValue:"", sValue:""}; 
     }
 
     handleChange = (e) => { e.preventDefault(); this.setState({value: e.target.value.toUpperCase()})}
 
-    handleSubmit = e => {e.preventDefault(); alert(`Se ha decidido el nombre ${this.state.value}`)}
-    
+    handleSubmit = e => {e.preventDefault(); alert(`Se ha decidido el nombre ${this.state.iValue}`)}
+
+    handleSelect = e => {e.preventDefault(); this.setState({sValue: e.target.value})}
+
     render(): React.ReactNode {
         return (
         <form onSubmit={this.handleSubmit}>
             <label> Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <input type="text" value={this.state.iValue} onChange={this.handleChange}/>
             </label>
+            <select value={this.state.sValue} onChange={this.handleSelect}>
+                <option value="orange">Orange</option>
+                <option value="apple">Apple</option>
+                <option value="pear">Pear</option>
+                <option value="grape">Grape</option>
+                <option value="melon">Melon</option>
+                <option value="waterMelon">Water Melon</option>
+            </select>
             <input type="submit" value="SUBMIT" />
         </form>
         );
